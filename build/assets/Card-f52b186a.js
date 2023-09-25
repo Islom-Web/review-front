77c6b703d22e4035717521c7e691468415bb56dc
Islom <lingualeo3007@gmail.com>
2023-09-25 10:54:42 +0500

    rebuilding project

--- build/assets/Card-f52b186a.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-6f7faf75.js";const r=s=>a.jsx("div",{className:`card ${s.className}`,style:s.style,children:s.children});export{r as C};
---
+++ dist/assets/Card-b927284e.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-962aa7a8.js";const r=s=>a.jsx("div",{className:`card ${s.className}`,style:s.style,children:s.children});export{r as C};
